6e79cf6c0aca51f1ec475e7764caa46b417b5d69
Erofey Bashunov <cannor147@gmail.com>
2022-09-26 18:48:10 +0300

    feat(linear): added generator and parity check matrix conversion

--- conspect/coding/linear/__init__.py
+++ conspect/coding/linear/__init__.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+from algebra.binary import binary_array
+from algebra.matrix import gaussian, transpose, from_permutation
 from coding.linear.gilbert_varshamov import gilbert_varshamov_bound_for_k, gilbert_varshamov_bound_for_d, \
     gilbert_varshamov_bound_for_n
 from coding.linear.griesmer import griesmer_bound_for_k, griesmer_bound_for_d, griesmer_bound_for_n
@@ -5,7 +9,31 @@ from coding.linear.hamming import hamming_bound_for_k, hamming_bound_for_d, hamm
 from coding.linear.singleton import singleton_bound_for_k, singleton_bound_for_d, singleton_bound_for_n
 
 
-def min_n(q, k, d, bound="hamming"):
+def gen_to_check(g):
+    n = len(g[0])
+    k = len(g)
+
+    (gs, p) = gaussian(g)
+    s = gs[:, k:n]
+
+    hs = np.append(-transpose(s), np.identity(k), axis=1)
+    return np.dot(hs, from_permutation(p))
+
+
+def check_to_gen(h):
+    n = len(h[0])
+    k = n - len(h)
+
+    (hs, p) = gaussian(h)
+    hs = np.roll(hs, k - n, axis=1)
+    p = np.roll(p, k - n)
+    s = hs[:, 0:n - k]
+
+    gs = np.append(binary_array(np.identity(k)), -transpose(s), axis=1)
+    return np.dot(gs, from_permutation(p))
+
+
+def min_n(q, k, d, bound):
     if bound == "singleton":
         return singleton_bound_for_n(k=k, d=d)
     if bound == "hamming":
@@ -17,7 +45,7 @@ def min_n(q, k, d, bound="hamming"):
     return None
 
 
-def max_k(q, n, d, bound="hamming"):
+def max_k(q, n, d, bound):
     if bound == "singleton":
         return singleton_bound_for_k(n=n, d=d)
     if bound == "hamming":
@@ -29,7 +57,7 @@ def max_k(q, n, d, bound="hamming"):
     return None
 
 
-def max_d(q, n, k, bound="hamming"):
+def max_d(q, n, k, bound):
     if bound == "singleton":
         return singleton_bound_for_d(n=n, k=k)
     if bound == "hamming":
