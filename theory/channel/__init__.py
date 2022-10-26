8cb433869f6b2f1dd4dd28b6266b0304747235c6
Erofey Bashunov <cannor147@gmail.com>
2022-10-26 11:34:58 +0300

    feat(theory): added section about bhattacharyya parameter

--- theory/channel/__init__.py
+++ theory/channel/__init__.py
@@ -5,5 +5,13 @@ def bsc_probability(x, y, p):
     return 1 - p if x == y else p
 
 
+def bsc_bhattacharyya(p):
+    return 2 * p - 2 * p * p
+
+
 def awgnc_probability(x, y, sigma):
     return math.exp(- ((y - x) / sigma) ** 2 / 2) / math.sqrt(2 * math.pi * sigma ** 2)
+
+
+def awgnc_bhattacharyya(sigma):
+    return math.exp(-0.5 / (sigma * sigma))
