d07a8f73af0aa4613ef87e92e39906265a7ef0b8
Erofey Bashunov <cannor147@gmail.com>
2022-10-27 11:15:14 +0300

    feat(theory): added section about binary erasing channel

--- theory/channel/__init__.py
+++ theory/channel/__init__.py
@@ -9,6 +9,14 @@ def bsc_bhattacharyya(p):
     return 2 * p - 2 * p * p
 
 
+def bec_probability(x, y, p):
+    return p if y == 'ϵ' else 1 - p if x == y else 0
+
+
+def bec_bhattacharyya(p):
+    return p
+
+
 def awgnc_probability(x, y, sigma):
     return math.exp(- ((y - x) / sigma) ** 2 / 2) / math.sqrt(2 * math.pi * sigma ** 2)
 
